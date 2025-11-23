9238ead4f988b989d89d3c8812e7a15964f120c1
Emanuel Lopes Ferreira <evlf@outlook.pt>
2025-11-23 14:33:22 +0000

    now much cleaner

--- __main__.py
+++ __main__.py
@@ -11,10 +11,19 @@ from persistence.database import MariaDBHandler
 
 CAMERA_CONNECTION_ATTEMPTS_LIMIT = 3
 TEMPERATURE_THRESHOLD = 10
+"""
 
+Aplicação gloriosa que lê o leitor das caldeiras Ferlux e 
+transforma a informação do visor em extraordinarios dados. 
+
+Usa CV2 e Tesseract OCR para operar a magia sobre a imagem e 
+depois inserir esses dados numa base de dados. Bastante inutil mas um exercicio divertido. 
+
+"""
 def form_database_connection(user : str, pwd : str, host : str, db : str):
     database_url = f"mariadb+mariadbconnector://{user}:{pwd}@{host}/{db}"
     return database_url
+
 def process_image(image, is_debug):
     gray_frame = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     ret, image_to_test = cv2.threshold(gray_frame, 240, 255, cv2.THRESH_BINARY_INV)    
@@ -23,19 +32,22 @@ def process_image(image, is_debug):
         cv2.waitKey(0)
 
     return image_to_test
+
 def form_source_endpoint(ip : str, port : str) -> str:
     endpoint = f"http://{ip}:{port}/shot.jpg"
     return endpoint
+
 def get_settings(file_name : str) -> dict:
     settings = None
     with open(file_name, 'r') as file:
         settings = json.load(file)
     return settings
+
 def extract_text(frame, is_debug):
-    # Use pytesseract to do OCR on the processed frame
     image_to_parse = process_image(frame, is_debug) 
     text = pytesseract.image_to_string(image_to_parse, lang='lets', config="--oem 3 --psm 6 -c tessedit_char_whitelist=aA1234567890")
     return text.strip()
+
 def cleanup(capture=None):
     if capture is not None:
         capture.release()
@@ -50,11 +62,11 @@ def main():
 
     args = parser.parse_args()
     
+    # definições
     app_settings = get_settings(f"{args.settings}.json")
-    # set tesseract dir
     pytesseract.pytesseract.tesseract_cmd = app_settings["ocr"]["tesseract-dir"]
     
-    # Logging info
+    # logger
     logging_level = logging.DEBUG if args.debug == True else logging.INFO
     main_logger = logging.getLogger("Boiler OCR")    
     
@@ -63,8 +75,7 @@ def main():
         logging.basicConfig(filename=fname, level=logging_level, format=f'%(asctime)s %(levelname)s: %(message)s')
     else:
         logging.basicConfig(level=logging_level, format=f'%(asctime)s %(levelname)s: %(message)s') 
-    
-    # create source video feed
+    # Base de dados e video
     source = form_source_endpoint(app_settings["camera"]["connection"]["ip"], app_settings["camera"]["connection"]["port"])
     database_url = form_database_connection(app_settings["app"]["database"]["user"],
                                             app_settings["app"]["database"]["password"],
@@ -73,7 +84,6 @@ def main():
                                             )
     wait_time = 0 if "wait" not in app_settings["app"] else app_settings["app"]["wait"]
 
-    # Capture video from a specified source (default is webcam)
     feed_live = True
     main_logger.info("Video feed started. Analyzing frames.")
     connection_attempts = 0
@@ -83,8 +93,9 @@ def main():
         db_handler = MariaDBHandler(database_url, main_logger)
     
     main_logger.debug(f"Trying to connect to {source}")
-
+    
     try:
+        # O smartphone fazia timeout se o objeto estivesse sempre instanciado
         while feed_live:
             capture = cv2.VideoCapture(source, cv2.CAP_FFMPEG)
             main_logger.debug("Reading frame")
@@ -101,20 +112,20 @@ def main():
                 capture = cv2.VideoCapture(source, cv2.CAP_FFMPEG)
                 capture.set(cv2.CAP_PROP_BUFFERSIZE, 1)                
                 continue
-
+            # Lê o frame
             ret, frame = capture.read()
             if not ret:
                 main_logger.critical("Couldn't read frame.")
                 return
             
-            # Extract text from the current frame
+            # Tesseract analisa a imagem e transforma numa string
             detected_text = extract_text(frame, args.debug)
             
-            # Parse the detected text (this is a basic example)
             main_logger.debug(f"Detected Text: {detected_text}")
             result = None
             try:
-                
+
+                # Instanciar. Validações estão dentro do objeto                
                 result = BoilerData(detected_text, main_logger, args.dry_run, db_handler)
                 
                 if result.temperature > TEMPERATURE_THRESHOLD:
@@ -123,8 +134,7 @@ def main():
             except Exception as e:
                 main_logger.warning(f"Failed while forming the log. Retrying in the next cycle {e}. OCR is {detected_text}")
             
-            capture.release()
-            cv2.destroyAllWindows()
+            cleanup(capture)
             main_logger.debug("Resources released. Waiting")
             time.sleep(wait_time)
 
